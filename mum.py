6b8efc1fa7061b278fd4521389337be48ffe6117
Clare Jarvis <clare@Alex-Aylesburys-MacBook.local>
2015-11-29 17:47:59 +0000

    first attempt

---
+++ mum.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+x=-1
+while  x!=0:
+	print('type in another number')
+	x=float(raw_input(''))
+
+print('your number is x')
+
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+This project is my new project to do maximum numbers
+Remember the highest number.
+
